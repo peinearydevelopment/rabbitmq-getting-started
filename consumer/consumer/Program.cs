20b8beda6a14a870fe9227c5d77d350df31a6148
peinearydevelopment <peinearydevelopment@gmail.com>
2017-03-20 15:55:15 -0400

    backup: test project from 2015

---
+++ consumer/consumer/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace consumer
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Text;
+	using System.Threading;
+	using RabbitMQ.Client;
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var factory = new ConnectionFactory { HostName = "localhost" };
+			using (var connection = factory.CreateConnection())
+			{
+				using (var channel = connection.CreateModel())
+				{
+					channel.QueueDeclare("task_queue", true, false, false, null);
+					channel.BasicQos(0, 1, false);
+
+					var consumer = new QueueingBasicConsumer(channel);
+					//var arguments = new Dictionary<string, object> { { "x-priority", 10 } };
+					//channel.BasicConsume("task_queue", true, "consumer-tag", arguments, consumer);
+					channel.BasicConsume("task_queue", true, consumer);
+
+					Console.WriteLine(" [*] Waiting for messages. \n To exit press CTRL+C");
+
+					while (true)
+					{
+						var ea = consumer.Queue.Dequeue();
+
+						var body = ea.Body;
+						var message = Encoding.UTF8.GetString(body);
+						Console.WriteLine(" [x] Received {0}", message);
+
+						var dots = message.Split('.').Length - 1;
+						Thread.Sleep(1 * 1000);
+
+						Console.WriteLine(" [x] Done");
+
+						//channel.BasicAck(ea.DeliveryTag, true);
+					}
+				}
+			}
+		}
+	}
+}
---
+++ producer/producer/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace producer
+{
+	using System;
+	using System.Text;
+	using RabbitMQ.Client;
+
+	class Program
+	{
+		private static int index = 0;
+		private static byte b;
+		static void Main(string[] args)
+		{
+			var factory = new ConnectionFactory { HostName = "localhost" };
+			using (var connection = factory.CreateConnection())
+			{
+				using (var channel = connection.CreateModel())
+				{
+					channel.QueueDeclare("task_queue", true, false, false, null);
+
+					for (var i = 0; i < 1000; i++)
+					{
+						b = (byte)(index % 9);
+						var message = GetMessage(args);
+						var body = Encoding.UTF8.GetBytes(message);
+
+						var properties = channel.CreateBasicProperties();
+						properties.Priority = b;//(byte)(index % 9);
+						properties.DeliveryMode = 2;
+						properties.SetPersistent(true);
+
+						channel.BasicPublish("", "task_queue", properties, body);
+						Console.WriteLine(" [x] Sent {0}", message);
+						index++;
+					}
+				}
+			}
+		}
+
+		private static string GetMessage(string[] args)
+		{
+			return ((args.Length > 0) ? string.Join(" ", args) : b + " Hello World!");
+		}
+	}
+}
